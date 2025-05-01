d4ef6123190e40a3a87ed358753eb2482f5da9c5
bushrahaafs <bushrahafs@gmail.com>
2025-05-01 19:08:53 +0300

    API

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = 'https://localhost:7237/api/auth';
+const API = `${import.meta.env.VITE_API_BASE_URL}/auth`;
 
 export const registerUser = async (data: { name: string; email: string; password: string }) => {
   const response = await axios.post(`${API}/register`, data);
@@ -11,5 +11,6 @@ export const loginUser = async (data: { email: string; password: string }) => {
   const response = await axios.post(`${API}/login`, data);
   return response.data;
 };
+
 
 
